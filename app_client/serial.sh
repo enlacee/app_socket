3bbd95672acf32f65cf2872e6541fc73b7044aa8
Anb ubuntu 10.04lts <detops.tk@gmail.com>
2014-04-30 13:36:34 -0500

    update cliente

--- app_client/serial.sh
+++ app_client/serial.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 RUTA_PS=/dev/ttyS1 #Puerto Serial
-
 CONF_VELOCIDAD="9600"
 CONF_NUMERO_BITS_TRAMA="8"
 CONF_OPCIONES="-parenb"
+RUTA_PHP=application/client.php
+
+echo "######### en dev ##########"
+echo "leyendo puerto : $RUTA_PS"
+sleep(1)
 
 #Configurando puerto serial para lectura
 stty $CONF_VELOCIDAD -F $RUTA_PS cs$CONF_NUMERO_BITS_TRAMA $CONF_OPCIONES
 
 #Capturando data
-cat $RUTA_PS | while read line; do php serial.php "${line}" && echo ${line}; done >> serial.log &
-
-
-#FUNCIONA lunes 28 : 11:53 2014
-#stty 9600 -F /dev/ttyS1 cs8 -parenb
+cat $RUTA_PS | while read line; do php RUTA_PHP "${line}" && echo ${line}; done >> serial.log &
+#cat $RUTA_PS | while read line; do echo $line; done >> serial.log &
