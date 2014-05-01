298f1dd1093f2fc1cfc23ad38d4be6824f9f1834
Anibal Copitan <acopitan@gmail.com>
2014-05-01 13:58:06 -0500

    port 81

--- app_server/application/server.php
+++ app_server/application/server.php
@@ -9,7 +9,7 @@
  */
 
 # server.php
-$server = stream_socket_server("tcp://127.0.0.1:1337", $errno, $errorMessage);
+$server = stream_socket_server("tcp://127.0.0.1:81", $errno, $errorMessage);
 
 if ($server === false) {
     throw new UnexpectedValueException("Could not bind to socket: $errorMessage");
