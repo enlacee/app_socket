5658b890beba2722e2e0d56607212f97c99f71f5
Anb <detops.tk@gmail.com>
2014-05-18 13:27:28 -0500

    up

--- graficos/parametros/parametros_view6.php
+++ graficos/parametros/parametros_view6.php
@@ -52,74 +52,66 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3 column">
-                    <div class="row">
-                        <label class="col-sm-12 control-label">Seleccione Parametros</label>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="param_1" id="tipo_variable" class="form-control input-sm">
-                                <option value="" selected="selected">- select 1</option>
-                                <option value="1">entero</option>
-                                <option value="2">real</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="param_1" id="tipo_variable" class="form-control input-sm">
-                                <option value="" selected="selected">- select 2</option>
-                                <option value="1">entero</option>
-                                <option value="2">real</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="param_1" id="tipo_variable" class="form-control input-sm">
-                                <option value="" selected="selected">- select 3</option>
-                                <option value="1">entero</option>
-                                <option value="2">real</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="param_1" id="tipo_variable" class="form-control input-sm">
-                                <option value="" selected="selected">- select 4</option>
-                                <option value="1">entero</option>
-                                <option value="2">real</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="param_1" id="tipo_variable" class="form-control input-sm">
-                                <option value="" selected="selected">- select 5</option>
-                                <option value="1">entero</option>
-                                <option value="2">real</option>
-                            </select>
-                        </div>
-                    </div>
+                    <form name = "graf1" id="graf1">
+                        <fieldset name = "fieldset" id = "fieldset">
+                            <div class="row">
+                                <label class="col-sm-12 control-label">Seleccione Parametros</label>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="param_1" id="param_1" class="form-control input-sm param">
+                                        <option value="" selected="selected">- select 1</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="param_2" id="param_2" class="form-control input-sm param">
+                                        <option value="" selected="selected">- select 2</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="param_3" id="param_3" class="form-control input-sm param">
+                                        <option value="" selected="selected">- select 3</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="param_4" id="param_4" class="form-control input-sm param">
+                                        <option value="" selected="selected">- select 4</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="param_5" id="param_5" class="form-control input-sm param">
+                                        <option value="" selected="selected">- select 5</option>
+                                    </select>
+                                </div>
+                            </div>
 
-                    <div class="row form-group">
-                        <label class="control-label col-sm-6 col-xs-6" for="total_depth">Total depth</label>
-                        <div class="col-sm-6 col-xs-6">
-                            <select name="total_depth" id="total_depth" class="form-control input-sm">
-                                <option value="" selected="selected">- select</option>
-                                <option value="10">10</option>
-                                <option value="25">25</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                                <option value="250">250</option>
-                                <option value="500">500</option>
-                                <option value="1000">1000</option>
-                            </select>
-                        </div>
-                    </div>
+                            <div class="row form-group">
+                                <label class="control-label col-sm-6 col-xs-6" for="total_depth">Total depth</label>
+                                <div class="col-sm-6 col-xs-6">
+                                    <select name="total_depth" id="total_depth" class="form-control input-sm">
+                                        <option value="" selected="selected">- select</option>
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                        <option value="250">250</option>
+                                        <option value="500">500</option>
+                                        <option value="1000">1000</option>
+                                    </select>
+                                </div>
+                            </div>
 
-                    <div class="row form-group">
-                        <div class="col-sm-12 col-xs-12">
-                            <button id="btnSee" type="button" class="btn btn-primary col-sm-12 col-xs-12">Ver</button>
-                        </div>
-                    </div>
-
-
+                            <div class="row form-group">
+                                <div class="col-sm-12 col-xs-12">
+                                    <button id="btnSee" type="button" class="btn btn-primary col-sm-12 col-xs-12">Ver</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
 
                     <!-- lista de parametros -->
                     <hr>
@@ -202,6 +194,75 @@
         <script type="text/javascript" src="../public/vendor/jquery/jquery-1.11.0.min.js"></script>
         <script type="text/javascript" src="../public/bootstrap/js/bootstrap.min.js"></script>
         <script type="text/javascript">
+    // ----- init Class
+    //Constructor
+    function Slot() {
+        //Atributos de la clase
+        this.vars = {
+            version : 1.0,
+            url : 'parametros.php',
+            // variable form
+            fieldset : '#fieldset',
+            param : '.param',
+        };
+    }
+    //Getters y Setters
+    Slot.prototype.get = function(atribute) {
+        switch(atribute)
+        {
+            case 'url':
+                return this.vars.url;
+            case 'version':
+                return this.vars.version;
+            case 'fieldset':
+                return this.vars.fieldset;
+            case 'param':
+                return this.vars.param;
+        }
+    };
+    Slot.prototype.set = function(atribute, value) {
+        switch(atribute)
+        {
+            case 'url':
+                this.vars.url = value;
+                break;
+            case 'atributo2':
+                this.vars.version = value;
+                break;
+        }
+    };   
+    Slot.prototype.listOption = function() {
+        var self = this,
+        $fieldset = $(self.get('fieldset')),
+        $param = $(self.get('param'));
+        
+        $fieldset.attr('disabled',true);        
+        $.ajax({
+            url: self.get('url'),
+            data : {op : 'listaCombo_json'},
+            type: 'GET',
+            dataType: 'json',
+            success: function (data){
+                for (var i = 1; i <= $param.length; i++) {
+                    $.each(data, function(key, value) {
+                        if (value.slot != '0108') {
+                            $('#param_'+i)
+                                .append($("<option></option>")
+                                .attr("value", value.slot)
+                                .text(value.name));
+                        }
+                    });
+                }
+                $fieldset.attr('disabled',false);
+            }
+        });
+        
+    }
+    // ----- End Class
+
+    // application
+    var obj = new Slot();    
+    obj.listOption();
         </script>
     </body>
 </html>
